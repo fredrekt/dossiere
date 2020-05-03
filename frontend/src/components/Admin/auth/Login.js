02008dae038b699c812a817a0b6378691a45c818
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-04 00:44:16 +0800

    changed the vector image

--- frontend/src/components/Admin/auth/Login.js
+++ frontend/src/components/Admin/auth/Login.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { MDBContainer, MDBRow, 
     MDBCol, MDBBtn, MDBInput } from 'mdbreact';
-import frontdesk from '../../../img/frontdesk-vector.png'
+import frontdesk from '../../../img/frontdesk-vector.jpg'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+    const [ formData, setFormData ] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = async e =>{
+        e.preventDefault();
+        console.log("Submitted")
+    }
+
   return (
       <MDBContainer style={{'margin-top':'13%'}}>
         <MDBRow>
@@ -12,21 +28,38 @@ const Login = () => {
             </MDBCol>
             <MDBCol md="6">
                 <MDBContainer>
-                    <form>
+                    <form onSubmit={e => onSubmit(e)}>
                         <h3 style={{ fontSize: "40px" }} className="h5 mb-4">Sign in</h3>
                         <p style={{ fontSize: "20px" }} className="grey-text">
                             Sign In to proceed to your dashboard
                         </p>
                         <div className="grey-text">
-                        <MDBInput className="w-100" label="Type your email" group type="email" validate error="wrong"
+                        <MDBInput 
+                            className="w-100" 
+                            label="Type your email" 
+                            group 
+                            type="email" 
+                            validate 
+                            name="email"
+                            value={email}
+                            onChange={e => onChange(e)}
+                            error="wrong"
                             success="right" />
-                        <MDBInput className="w-100" label="Type your password" group type="password" validate />
+                        <MDBInput 
+                            className="w-100" 
+                            label="Type your password" 
+                            group 
+                            name="password"
+                            value={password}
+                            onChange={e => onChange(e)}
+                            type="password" 
+                            validate />
                         </div>
                         <div className="ml-0 mb-4">
-                            Forgot your password? <a href="#retrieve">Retrieve here</a>
+                            Forgot your password? <Link to="/register">Retrieve here</Link>
                         </div>
                         <div className="text-center">
-                        <MDBBtn className="w-100 ml-0">Login</MDBBtn>
+                        <MDBBtn type="submit" className="w-100 ml-0">Login</MDBBtn>
                         </div>
                     </form>
                 </MDBContainer>
