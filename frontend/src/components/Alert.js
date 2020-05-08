de106e9837be4b58a5d5d156670b8b303e1363a2
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-08 22:08:30 +0800

    used react-toastify - for notifications and error handling (login)

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -2,15 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { MDBAlert, MDBContainer } from 'mdbreact'
+import 'react-toastify/dist/ReactToastify.css'
+import { toast } from 'react-toastify'
 
+toast.configure()
+
+const showToast = ( toastMsg ) => {
+    toast.error(toastMsg)
+}
 const Alert = ({ alerts }) => 
     alerts !== null && 
     alerts.length > 0 && 
     alerts.map(alert => (
     <MDBContainer>
-        <MDBAlert dismiss key={alert.id} color={alert.alertType} >
+        {/* <MDBAlert dismiss key={alert.id} color={alert.alertType} >
             {alert.msg}
-        </MDBAlert>
+        </MDBAlert> */}
+        {showToast(alert.msg)}
     </MDBContainer>
 ))
 
