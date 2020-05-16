3e3c65b39cc1588a0a8ce82ec2179347a430c5cd
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-17 03:57:04 +0800

    blog main (container of posts/blogs/articles) and icon for blogs based on topics and limited display (preview of content)

--- frontend/src/components/BlogMain.js
+++ frontend/src/components/BlogMain.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { MDBContainer, MDBRow, MDBCol, MDBView, MDBMask, MDBBtn, MDBIcon } from 'mdbreact'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 
 const BlogMain = props => {
     const data = props.data
     const blogData = data.map((post)=>
          <MDBRow className="mt-5 mb-5" key={post._id}>
+           <hr className="my-5" />
           <MDBCol lg="5">
             <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
               <img
@@ -20,15 +23,15 @@ const BlogMain = props => {
           <MDBCol lg="7">
             <a href="#!" className="green-text">
               <h6 className="font-weight-bold mb-3">
-                <MDBIcon icon="utensils" className="pr-2" />
-                Food
+                {post.topic === 'Food' ? <MDBIcon icon="utensils" className="pr-2" /> : post.topic === 'News' ? <MDBIcon far icon="newspaper" className="pr-2" /> : post.topic === 'Work' ? <MDBIcon icon="briefcase" className="pr-2" /> : <MDBIcon icon="code" className="pr-2"/>}
+                {!post.topic ? 'Food' : post.topic}
               </h6>
             </a>
             <h3 className="font-weight-bold mb-3 p-0">
               <strong>{post.title}</strong>
             </h3>
            
-            <p>
+            <p class="blog-post-content">
               {post.content}
             </p>
             <div>
@@ -36,12 +39,15 @@ const BlogMain = props => {
               <a href="#!">
                 <strong>{post.user}</strong>
               </a>
-              <div style={{ fontSize: "small" }}>{post.date}</div>
+              <div className="text-capitalize" style={{ fontSize: "small" }}>
+                <Moment format="MMM DD, YYYY">{post.date}</Moment>
+              </div>
             </div>
-            <MDBBtn href="/blog/3" color="success" size="md" className="waves-light ">
-              Read more
+            <MDBBtn color="success" size="md" className="waves-light ml-0 mt-4">
+              <Link className="white-text" to={`/blog/${post._id}`}>Read more</Link>
             </MDBBtn>
           </MDBCol>
+          <hr className="my-5" />
         </MDBRow>
     )
     return (
