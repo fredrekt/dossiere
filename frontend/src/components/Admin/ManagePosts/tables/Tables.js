29128c1807cfcfa5061df9932e8c237df7b6f5c4
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-21 21:50:00 +0800

    added datatables for the posts/articles/blogs for admin

---
+++ frontend/src/components/Admin/ManagePosts/tables/Tables.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { MDBTable, MDBTableBody, MDBTableHead,
+    MDBIcon, MDBBtn, MDBContainer, MDBModal, MDBModalHeader, MDBModalFooter, MDBModalBody } from 'mdbreact';
+import Moment from 'react-moment'
+import { connect } from 'react-redux'
+import { deletePost } from '../../../../actions/post'
+
+const Tables = ({ data, deletePost }) => {
+
+    const [ show, setShow ]= useState(false)
+
+    const tableData = data.map((datas)=>
+
+        <>
+            <tr>
+                <td>{datas._id}</td>
+                <td>{datas.title}</td>
+                <td>{datas.views.length === 0 ? 'No views yet' :datas.views.length}</td>
+                <td>{datas.likes.length === 0 ? 'No likes yet' : datas.likes.length}</td>
+                <td>{datas.comments.length === 0 ? 'No comments yet': datas.comments.length}</td>
+                <td>{datas.date}</td>
+                <td><MDBBtn onClick={(e)=>{
+                    e.preventDefault()
+                    deletePost(datas._id)
+                    }} color="danger" size="sm"><MDBIcon far icon="trash-alt" className="danger-text"/></MDBBtn></td>
+            </tr>
+        </>
+    )
+    return (
+        <>
+            <MDBTable hover>
+            <MDBTableHead>
+                <tr>
+                <th>Article ID</th>
+                <th>Title</th>
+                <th>Views</th>
+                <th>Likes</th>
+                <th>Comments</th>
+                <th>Date Published</th>
+                <th>Action</th>
+                </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+                <>
+                    {tableData}
+                </>
+            </MDBTableBody>
+            </MDBTable>
+        </>
+    )
+}
+
+Tables.propTypes = {
+    deletePost: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+
+export default connect(mapStateToProps, { deletePost })(Tables)
