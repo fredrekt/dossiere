1482f770c027f2e819848a6c9144ca019e093ddd
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-21 21:46:21 +0800

    Single Blog (Blog Page Carousel) done -> implement tree for comment and reply to comment

--- frontend/src/components/BlogPageCarousel.js
+++ frontend/src/components/BlogPageCarousel.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MDBContainer, MDBCard, MDBCardBody, MDBIcon,
-    MDBBox, MDBCardText } from "mdbreact"; 
+    MDBBox, MDBCardText, MDBBtn, MDBInput } from "mdbreact"; 
 import Comments from './Comments'
 import pp from '../img/fred.jpg'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getPost, addLike, removeLike } from '../actions/post'
+import Moment from 'react-moment'
+import CreateComment from './CreateComment';
+import Lottie from 'lottie-react-web'
+import heart from '../img/heart.json'
+import loadspinner from '../img/loading.json'
 
-const BlogPageCarousel = () => {
-    return (
-        <div>
+const BlogPageCarousel = ({ getPost, addLike, removeLike, post: { post, loading }, match }) => {
+    
+    useEffect(()=>{
+        getPost(match.params.id)
+    }, [getPost])
+
+    const [ play, setPlay ] = useState(false) 
+    
+    return loading || post === null ? 
+    (
+        <>
+        <Lottie
+            options={{
+                animationData: loadspinner
+            }}
+            width = "45%"
+            height = "45%"
+        />
+        <div style={{ marginTop: "0" }} className="text-center grey-text">
+            Taking you to the skies or wherever you want to go
+        </div>
+        </>
+    )
+    :
+    (
+        <>
             {/* <Breadcrumbs
                 bcrumbs_color="white"
                 bcrumbs_header="My Blogs"
@@ -14,11 +45,14 @@ const BlogPageCarousel = () => {
                 /> */}
             <div className="blog-carousel-container p-5">
                 <div className="blog-carousel-title p-2">
-                    How hard to be a Full Stack Developer?
+                    {post ? post.title : ' How hard to be a Full Stack Developer?'}
                 </div>
                 <div className="blog-carousel-date-container p-2">
                     <div className="blog-carousel-date">
-                        AUG 18, 2020
+                        {post ? 
+                        <Moment format="MMM DD, YYYY">{post.date}</Moment>
+                        : 'AUG 18, 2020'
+                        }
                     </div>
                 </div>
             </div>
@@ -31,7 +65,9 @@ const BlogPageCarousel = () => {
                     <a href="#imgcarousel" className="indigo-text mx-2">
                     <img src={pp} alt="img" className="z-depth-2"
                         style={{"width": "40px", "height": "40x","border-radius":"50px"}}/>
-                    <strong className="ml-1">Fred Garingo</strong>
+                    <strong className="ml-1">
+                        {post ? post.user : ' Fred Garingo' }
+                    </strong>
                     </a>
                 </strong>
                 </p>
@@ -49,12 +85,28 @@ const BlogPageCarousel = () => {
                     <i className="fab fa-linkedin-in"></i>
                 </button>
 
-                <button type="button" className="btn btn-sm btn-li my-0">
-                    <MDBIcon size="1x" className="red-text" icon="heart" />
+                <button onClick={(e)=>{
+                    setPlay(!play)
+                    {!play ? addLike(post._id) : removeLike(post._id) }
+                    }} type="button" className="btn btn-sm btn-li my-0">
+                    
+                   {!play ? 
+                   <MDBIcon className="grey-text" icon="heart"/> 
+                   : 
+                   <Lottie
+                        style={{ backgroundColor: null }}
+                        direction={play ? 1: -1}
+                        options={{
+                            animationData: heart,
+                            autoplay: false
+                        }}
+                        width = "20%"
+                        height = "20%"
+                    />}
                 </button>
 
                 <a role="button" className="btn btn-sm btn-unique my-0" href="#section-comments">
-                    <i className="far fa-comments mr-1"></i> Comments <span>(4)</span>
+                    <i className="far fa-comments mr-1"></i> Comments {post ? `(${post.comments.length})` :<span>(4)</span>}
                 </a>
                 </div>
 
@@ -79,10 +131,19 @@ const BlogPageCarousel = () => {
                   <MDBBox className="p-4" display="flex" justifyContent="center">
                     <MDBContainer>
                         <div className="blog-carousel-update">
-                            UPDATED 4:09 PM
+                            {post ? 
+                            <>
+                            <span>UPDATED </span>
+                            <Moment format="hh:mm A">
+                                {post.date}
+                            </Moment>
+                            </>
+                            :
+                            'UPDATED 4:09 PM'
+                            }
                         </div>
                     </MDBContainer>
-                    <MDBIcon className="blog-carousel-options mr-3" onClick={()=>alert("Clicked Options")} size="2x" icon="ellipsis-h" />
+                    <MDBIcon className="blog-carousel-o ptions mr-3" onClick={()=>alert("Clicked Options")} size="2x" icon="ellipsis-h" />
                   </MDBBox>
                 </MDBCard>
               </MDBContainer>
@@ -90,12 +151,59 @@ const BlogPageCarousel = () => {
             </section>
             </div>
             <MDBContainer>
-                <Comments/>
-                <Comments/>
-                <Comments/>
+                {/* <Comments key={post.comments._id} postId={post._id} comments={post.comments}/> */}
+                {post.comments.map((com)=>
+                <MDBBox className="pt-4 pb-4" display="flex">
+                    <MDBBox display="flex">
+                        {post.comments.user === null ? 
+                        <MDBIcon size="3x" icon="user-circle" />
+                        :
+                        <div className="comment-avatar">
+                            <img src={pp} alt=""/>  
+                        </div>  
+                        }            
+                    <MDBBox flex="column" display="flex" className="ml-3">
+                        <MDBBox alignItems="center" display="flex" justifyContent="between">
+                        <div className="comment-author">
+                            Fred Garingo
+                        </div>
+                        <div className="comment-reply">
+                            <MDBBtn color="info" outline size="sm">
+                            Reply
+                            </MDBBtn>
+                        </div>
+                        </MDBBox>
+                        <div className="comment-date mt-0">
+                        <Moment format="MMM DD, YYYY" >
+                            {com.date}
+                        </Moment>
+                        <span>: </span>
+                        <Moment fromNow>{com.date}</Moment>
+                        {/* AUG 28, 2019 20:29 */}
+                        </div>
+                        <div className="comment-content mt-3">
+                              {com.comment}  
+                        </div>
+                    </MDBBox>
+                    </MDBBox>
+                </MDBBox> 
+                )}
+
             </MDBContainer>
-        </div>
+            <CreateComment postId={post._id}/>
+        </>
     )
 }
 
-export default BlogPageCarousel
+BlogPageCarousel.propTypes = {
+    getPost: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    post: state.post
+})
+
+export default connect(mapStateToProps, { getPost, addLike, removeLike } )(BlogPageCarousel)
