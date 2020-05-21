1eb6d30a7d6a10c4a9c1752c930a05f5d4df944b
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-21 21:52:06 +0800

    i think need to implement map to map more all the comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { MDBBox, MDBIcon, MDBBtn } from 'mdbreact'
 import pp from '../img/fred.jpg'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Moment from 'react-moment'
 
-const Comments = (props) => {
+const Comments = ({ postId, comments: { _id, user, comment, date }, auth }) => {
     return (
         <>
           <MDBBox className="pt-4 pb-4" display="flex">
@@ -23,11 +26,16 @@ const Comments = (props) => {
                   </div>
                 </MDBBox>
                 <div className="comment-date mt-0">
+                  <Moment format="MMM DD, YYYY" fromNow>
+                    {date}
+                  </Moment>
                   AUG 28, 2019 20:29
                 </div>
                 <div className="comment-content mt-3">
-                Nam id pretium lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dui lectus.
+                {comment ? comment : 
+                ("Nam id pretium lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dui lectus.")
+                }
                 </div>
               </MDBBox>
               </MDBBox>
@@ -36,4 +44,14 @@ const Comments = (props) => {
     )
 }
 
-export default Comments
+Comments.propTypes = {
+  postId: PropTypes.number.isRequired,
+  comments: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {  })(Comments)
