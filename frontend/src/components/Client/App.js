d63b3fd225990a614037d0ca8152540656da2a56
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-21 21:45:29 +0800

    Client Navbar added portfolio and landing page and migrated successfully for the blog and blog/:id

--- frontend/src/components/Client/App.js
+++ frontend/src/components/Client/App.js
@@ -5,7 +5,11 @@ import Home from './Home'
 import BlogPage from '../BlogPage'
 import { MDBContainer } from 'mdbreact'
 import BlogGridView from '../BlogGridView'
-
+import BlogPageCarousel from '../BlogPageCarousel'
+import Landing from '../LandingPage/Landing'
+import Login from '../Admin/auth/Login'
+import Register from '../Admin/auth/Register'
+import Portfolio from './Home'
 
 // Application Visitor/Client 
 
@@ -16,11 +20,16 @@ const App = () => {
             <Router>
                 <Navbar/>
                 <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <MDBContainer style={{ marginBottom: "10%" }}>
+                    <Route exact path="/" component={Landing}/>   
+                    <Route exact path="/portfolio" component={Portfolio}/>            
+                    <MDBContainer style={{ marginBottom: "10%", marginTop: '10%' }}>
                         {/* Import path for the blogs  */}
-                        <Route exact path="/blog-gridview" component={BlogGridView}/>
-                    </MDBContainer>
+                        {/* <Route exact path="/blog-gridview" component={BlogGridView}/> */}
+                        <Route exact path="/blog" component={BlogPage}/>      
+                        <Route exact path="/blog/:id" component={BlogPageCarousel}/> 
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Register}/>  
+                    </MDBContainer> 
                 </Switch>
             </Router>
         </>
