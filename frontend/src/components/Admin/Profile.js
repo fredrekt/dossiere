cf96e56d1591ffd2277bd7be87b8363cc9d3547e
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-24 09:22:31 +0800

    started on frontend for the uploading of image

--- frontend/src/components/Admin/Profile.js
+++ frontend/src/components/Admin/Profile.js
@@ -464,10 +464,25 @@ const Profile = ({ profile: { profile, loading }, createProfile, getCurrentProfi
                             ):
                             exp===null ? 
                             (
-                            <>
-                                <div style={{ width: "200%" }}>
-                                    <MDBInput style={{ width: "100%" }} outline type="file" label="Add Image " />
+                            <>           
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="inputGroupFileAddon01">
+                                    Upload
+                                    </span>
                                 </div>
+                                <div className="custom-file">
+                                    <input
+                                    type="file"
+                                    className="custom-file-input"
+                                    id="inputGroupFile01"
+                                    aria-describedby="inputGroupFileAddon01"
+                                    />
+                                    <label className="custom-file-label" htmlFor="inputGroupFile01">
+                                    Choose file
+                                    </label>
+                                </div>
+                            </div>
                             </>
                             )
                             :(
