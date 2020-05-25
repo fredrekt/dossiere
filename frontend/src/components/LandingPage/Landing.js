5b1b8b5919776279fa68351f4233c756f49d9d2d
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-26 02:15:32 +0800

    landing page used scrollToComponent and change ('get started') button to route ('/login')

--- frontend/src/components/LandingPage/Landing.js
+++ frontend/src/components/LandingPage/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {  MDBContainer, MDBRow, MDBCol } from 'mdbreact'
 import blob from '../../img/blob-landing.svg'
 import Lottie from 'lottie-react-web'
@@ -12,8 +12,13 @@ import Services from './Services'
 import footerBg from '../../img/footer-bg.svg'
 import Slide from 'react-reveal'
 import { Link } from 'react-router-dom'
- 
-const Landing = () => {
+import scrollToComponent from 'react-scroll-to-component';
+
+const Landing = ({introSection}) => {
+
+    useEffect(()=>{
+        scrollToComponent(introSection, { offset: 0, align: 'top', duration: 2000, ease:'inSine'})
+    },[])
 
     const [ hover, setHover ] = useState(false)
 
@@ -45,6 +50,7 @@ const Landing = () => {
     return (
         <>
             <Slide cascade slide bottom>
+            <section ref={(section) => { introSection = section}}>
             <MDBContainer style={{ marginTop: "5%" }} fluid>
                 <div className="landing-wrapper">
                 <MDBRow>
@@ -54,14 +60,14 @@ const Landing = () => {
                                 Empowering Information.
                             </h1>
                             <h5 className="h5-responsive mb-4">
-                                Our landing page template works on all devices, so you only have to set 
+                                Our landing page works on all devices, so you only have to set 
                                 it up once, and get beautiful results forever. This Web Application is 
                                 fully made and designed by Fred Garingo. Took almost 2 months to finish 
                                 this startup project, hopefully it'll help and change lots of lives.
                             </h5>
-                            <Link className="link-landing-custom" to="/register">
+                            <Link className="link-landing-custom" to="/login">
                             <button onMouseEnter={()=>setHover(!hover)} onMouseLeave={()=>setHover(!hover)} className="z-depth-1 landing-btn" style={styledButton}>
-                                get powered up
+                                get started
                             </button>
                             </Link>
                         </MDBContainer>
@@ -80,6 +86,7 @@ const Landing = () => {
                 </MDBRow>
                 </div>
             </MDBContainer>  
+            </section>
             </Slide>
             <>
                 <Purpose/>
@@ -93,7 +100,7 @@ const Landing = () => {
             <div style={{ position: 'relative', marginTop: '15%', marginBottom: '-10%' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,32L48,42.7C96,53,192,75,288,112C384,149,480,203,576,197.3C672,192,768,128,864,112C960,96,1056,128,1152,133.3C1248,139,1344,117,1392,106.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
                 <div style={{ position: 'absolute', color: "#fff", marginBottom: '3%', left: 0, right: 0, bottom: 0, fontWeight: '900' }} className="text-center">
-                    Copyright {copyrightDate} | Fred Garingo
+                    Copyright {copyrightDate} | <a className="white-text" href="https://www.facebook.com/fredgaringo">Fred Garingo</a>
                 </div>
             </div>
         </>
