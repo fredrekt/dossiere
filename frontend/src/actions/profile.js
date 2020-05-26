df017eb2fc686e500736dedc4aafcf68c1896cee
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-27 06:39:10 +0800

    added function (redux action) to get all profiles of users

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
 import { setAlert } from './alert'
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED } from './types'
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED, GET_PROFILES } from './types'
+
+//get all user profiles
+export const getAllProfiles = () => async dispatch => {
+    try {
+      const res = await axios.get('/api/profile')  
+      
+      dispatch({
+          type: GET_PROFILES,
+          payload: res.data
+      })
+    } 
+    catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
 
 //get current user profile
 export const getCurrentProfile = () => async dispatch => {
