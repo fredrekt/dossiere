053f57f5b32cac5fe1e5d5ea80c3ef62d73d0457
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-05-27 04:32:24 +0800

    added Slide animation (react-reveal package) -> cascade slide bottom

--- frontend/src/components/Breadcrumbs.js
+++ frontend/src/components/Breadcrumbs.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { MDBMask } from 'mdbreact'
+import Slide from 'react-reveal'
 
 const Breadcrumbs = (props) => {
     return (
-        <div className="bcrumbs-size" >
-            <MDBMask overlay="black-strong" className="flex-center flex-column text-white text-center">
-                <div style={{'background':props.bcrumbs_color, backgroundImage: `url(${props.bcrumbs_image})`}} className="brc-img">
-                    <div className="brc-content">
-                        <div className="brc-ins-cont">
-                            <h1 className="brc-header">{props.bcrumbs_header}</h1>
-                            <a className='brc-bread-link' href="/">Home</a> <span className="black-text">/</span> <span className="grey-text">{props.bcrumbs_current}</span>
+        <>
+        <Slide cascade slide bottom>
+            <div className="bcrumbs-size" >
+                <MDBMask overlay="black-strong" className="flex-center flex-column text-white text-center">
+                    <div style={{'background':props.bcrumbs_color, backgroundImage: `url(${props.bcrumbs_image})`}} className="brc-img">
+                        <div className="brc-content">
+                            <div className="brc-ins-cont">
+                                <h1 className="brc-header">{props.bcrumbs_header}</h1>
+                                <a className='brc-bread-link' href="/">Home</a> <span className="black-text">/</span> <span className="grey-text">{props.bcrumbs_current}</span>
+                            </div>
                         </div>
-                    </div>
-                </div>      
-            </MDBMask>   
-        </div>
+                    </div>      
+                </MDBMask>   
+            </div>
+        </Slide>
+        </>
     )
 }
 
