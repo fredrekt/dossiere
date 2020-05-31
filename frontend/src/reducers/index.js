9e455c8ef6d0f17493df2869fa00c923ef035def
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-06-01 07:46:51 +0800

    added portfolio to reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,10 +3,12 @@ import alert from './alert'
 import auth from './auth'
 import profile from './profile'
 import post from './post'
+import portfolio from './portfolio'
 
 export default combineReducers({
     alert,
     auth,
     profile, 
-    post
+    post,
+    portfolio
 })
