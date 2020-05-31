d4f054f6b87f44c38a95b06e5ce32b74bc0550ee
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-06-01 07:50:18 +0800

    IntroSection of portfolio done (added bg blob to hero image & dailyHobby as props)

--- frontend/src/components/Client/sections/IntroSection.js
+++ frontend/src/components/Client/sections/IntroSection.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact'
-import pp from '../../../img/pp.jpg'
+import pp from '../../../img/pp-removebg-preview.png'
 import Typing from 'react-typing-animation';
+import introBgBlob from '../../../img/introBlob2.svg'
+import introWaves from '../../../img/introWavesBg.svg' 
 
 const IntroSection = (props) => {
     return (
         <>
+        <MDBContainer fluid style={{ backgroundImage: `url(${introWaves})`, backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat'}}>
         <MDBContainer>
                <MDBRow>
                    <MDBCol md="6">
@@ -21,7 +24,7 @@ const IntroSection = (props) => {
                             </Typing>
                                 <p className="portfolio-content">
                                     My name is {props.name ? props.name : 'not available'}, i currently live in {props.location ? props.location : 'location not available'}. 
-                                <br/>Donec vel dui lectus. Developer at heart and soul loves to watch netflix and develop shit.
+                                <br/>{props.dailyHobby ? props.dailyHobby : "Donec vel dui lectus. Developer at heart and soul loves to watch netflix and develop shit."}
                                 </p>
                             <div>
                                 <MDBBtn outline onClick={props.porfolioOnclick} size="md">
@@ -34,12 +37,13 @@ const IntroSection = (props) => {
                        </div>
                    </MDBCol>
                    <MDBCol md="6">
-                       <div className="portfolio-img">
+                       <div style={{ backgroundImage: `url(${introBgBlob})` }}  className="portfolio-img">
                            <img className="w-100 img-fluid" src={pp} alt=""/>
                        </div>
                    </MDBCol>
                </MDBRow>
             </MDBContainer> 
+            </MDBContainer>
         </>
     )
 }
