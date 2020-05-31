a58c927c0c60831ee24ad8e9cc7163708decd8b3
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-06-01 07:48:00 +0800

    modified contact model to be referred to portfolio(route) in Get In Touch section

--- models/Contact.js
+++ models/Contact.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose')
 
 const ContactSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-    },
-    subject:{
-        type: String,
-        required: true
-    },
-    message:{
-        type: String,
-        required: true
-    }
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        name:{
+            type: String,
+            required: true
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        subject:{
+            type: String,
+            required: true
+        },
+        message:{
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
 })
 
 module.exports = Contact = mongoose.model('contact', ContactSchema)
