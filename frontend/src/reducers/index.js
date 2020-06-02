80765bade919a7ac00dcdc366b6645982edfbc9a
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-06-02 12:42:19 +0800

    added messages reducer to reducer(combineReducers)

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -4,11 +4,13 @@ import auth from './auth'
 import profile from './profile'
 import post from './post'
 import portfolio from './portfolio'
+import messages from './messages'
 
 export default combineReducers({
     alert,
     auth,
     profile, 
     post,
-    portfolio
+    portfolio,
+    messages
 })
